77ccfe6f0950dfd1c63fd1355396d3835b49919e
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2025-10-27 03:41:56 +0800

    fix grpc transport `ENHANCE_YOUR_CALM` error
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- hotelReservation/dialer/dialer.go
+++ hotelReservation/dialer/dialer.go
@@ -34,8 +34,9 @@ func Dial(name string, opts ...DialOption) (*grpc.ClientConn, error) {
 
 	dialopts := []grpc.DialOption{
 		grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Timeout:             120 * time.Second,
-			PermitWithoutStream: true,
+			Time:                30 * time.Second,
+			Timeout:             5 * time.Second,
+			PermitWithoutStream: false,
 		}),
 	}
 	if tlsopt := tls.GetDialOpt(); tlsopt != nil {
